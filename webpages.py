a01b0bda739bdf5eebf0af0a949a2bf372f30c7c
AYUSH MISHRA <ayushmishra312004@gmail.com>
2023-03-10 22:57:28 +0530

    Sign-up-page

---
+++ webpages.py
@@ -0,0 +1,13 @@
+'''
+description about website
+sign up
+login 
+home page
+items page 
+description page about the item 
+sell itm register itm
+add to cart page ...itm we picked up
+payment page
+succefull payment page 
+
+'''
